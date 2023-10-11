c875f09b527319e7a118c6d8cf32d1bbdcd41a2b
xuebusi <xuebusi@qq.com>
2023-10-11 20:46:55 +0800

    在场景中间显示一个矩形

--- GameIntro/GameScene.swift
+++ GameIntro/GameScene.swift
@@ -10,5 +10,12 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         print("Game Scene Loaded")
+        
+        let square = SKSpriteNode()
+        square.size = CGSize(width: 50, height: 50)
+        square.position = CGPoint(x: size.width/2, y: size.height/2)
+        square.color = .green
+        
+        addChild(square)
     }
 }
