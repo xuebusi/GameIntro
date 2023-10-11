103536ae3ce0691700b547a2cced7d1096617e52
xuebusi <xuebusi@qq.com>
2023-10-11 20:29:27 +0800

    init

---
+++ GameIntro/GameIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameIntroApp.swift
+//  GameIntro
+//
+//  Created by shiyanjun on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct GameIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameIntro/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  GameIntro
+//
+//  Created by shiyanjun on 2023/10/11.
+//
+
+import Foundation
+import SpriteKit
+
+class GameScene: SKScene {
+    
+}
