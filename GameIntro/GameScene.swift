62d5383fc7955a65ebdd1c7fa8566ec30fb8ad7f
xuebusi <xuebusi@qq.com>
2023-10-11 20:53:09 +0800

    触摸操作

--- GameIntro/GameScene.swift
+++ GameIntro/GameScene.swift
@@ -8,14 +8,23 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let square = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
         print("Game Scene Loaded")
-        
-        let square = SKSpriteNode()
+        // 1. create a sprite
         square.size = CGSize(width: 50, height: 50)
         square.position = CGPoint(x: size.width/2, y: size.height/2)
         square.color = .green
         
         addChild(square)
+    }
+    
+    // 2. Touch Location
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            square.position = location
+        }
     }
 }
